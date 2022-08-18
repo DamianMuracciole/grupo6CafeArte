d74eb88daf4c01eee9d4b6633a6add1cd038fc01
gastonh017 <gaston.heiler@gmail.com>
2022-08-18 08:36:36 -0300

    api usuarios terminadas

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const path = require("path")
 const bcryptjs = require('bcryptjs');
-const { validationResult } = require('express-validator');
+const {
+    validationResult
+} = require('express-validator');
 const db = require('../database/models/index');
 const User = require("../database/models/User");
 const sequelize = db.sequelize
@@ -10,164 +12,218 @@ const Rols = db.Rol;
 //const { Op } = require("sequelize");
 
 const userController = {
-    
-    login: (req, res) => {
-        res.render("users/login")
-        
-    },    
-    loginProcess: (req, res) => {
-        // let userToLogin = {}
-        Users.findOne({
-            where: {
-                email: req.body.email
-            }
-        }).then((resultado) => {
-            userToLogin = resultado
-            if (userToLogin) {
-                let isOkcontrasena = bcryptjs.compareSync(req.body.password, userToLogin.password)
-                if (isOkcontrasena) {
-                    // si esta todo bien, quiero guardar el usuario en sesion, borrando la contraseña
-                    delete userToLogin.password;
-                    req.session.userLogged = userToLogin
-                    // en sesion tengo una propiedad llamada userLogged, que tiene toda la info de userToLogin 
-                    if (req.body.recordame) {
-                        res.cookie('userEmail', req.body.email, {
-                            maxAge: (1000 * 60) * 120 //ciento veinte minutos, un minuto por 120, 2 horas
+
+        login: (req, res) => {
+            res.render("users/login")
+
+        },
+        loginProcess: (req, res) => {
+            // let userToLogin = {}
+            Users.findOne({
+                where: {
+                    email: req.body.email
+                }
+            }).then((resultado) => {
+                userToLogin = resultado
+                if (userToLogin) {
+                    let isOkcontrasena = bcryptjs.compareSync(req.body.password, userToLogin.password)
+                    if (isOkcontrasena) {
+                        // si esta todo bien, quiero guardar el usuario en sesion, borrando la contraseña
+                        delete userToLogin.password;
+                        req.session.userLogged = userToLogin
+                        // en sesion tengo una propiedad llamada userLogged, que tiene toda la info de userToLogin 
+                        if (req.body.recordame) {
+                            res.cookie('userEmail', req.body.email, {
+                                maxAge: (1000 * 60) * 120 //ciento veinte minutos, un minuto por 120, 2 horas
+                            })
+                        }
+
+                        return res.redirect("/usuarios/perfil")
+                    } else {
+                        return res.render('users/login', {
+                            errors: {
+                                user: {
+                                    msg: "Debes ingresar los datos correctamente"
+                                }
+                            }
                         })
                     }
-                    
-                    return res.redirect("/usuarios/perfil")
                 } else {
                     return res.render('users/login', {
                         errors: {
-                            user: {
+                            email: {
                                 msg: "Debes ingresar los datos correctamente"
                             }
                         }
                     })
                 }
-            } else {
-                return res.render('users/login', {
-                    errors: {
-                        email: {
-                            msg: "Debes ingresar los datos correctamente"
-                        }
+            }).catch(err => {
+                res.send(err)
+            })
+        },
+        profile: (req, res) => {
+            res.render("users/profile", {
+                user: req.session.userLogged
+            });
+        },
+        logout: (req, res) => {
+            res.clearCookie('userEmail')
+            req.session.destroy();
+            return res.redirect("/")
+        },
+        register: (req, res) => {
+            res.render('users/register')
+        },
+        processRegister: async (req, res) => {
+            const resultValidation = validationResult(req)
+
+            await Users.findOne({
+                    where: {
+                        email: req.body.email
                     }
                 })
-            }
-        }).catch(err => {
-            res.send(err)
-        })
-    },
-    profile: (req, res) => {
-        res.render("users/profile", {
-            user:req.session.userLogged
-        });
-    },
-    logout: (req, res) => {
-        res.clearCookie('userEmail')
-        req.session.destroy();
-        return res.redirect("/")
-    },
-    register: (req, res) => {        
-        res.render('users/register')
-    },
-    processRegister: async (req, res) => {
-        const resultValidation = validationResult(req)
-
-        await Users.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-            .then((resultado) => {
-                userInDB = resultado
-            })
-            .catch(err => res.send(err))
-        
-        //console.log("el usuario esta en db?***", userInDB);
-
-        if (resultValidation.errors.length > 0) {
-            res.render('users/register', {
-                errors: resultValidation.mapped(),
-                oldData: req.body
-            });
-        } else if(userInDB) {
-            return res.render('users/register', {
-				errors: {
-					email: {
-						msg: 'Este email ya está registrado'
-					}
-				},
-				oldData: req.body
-			});
-        } else {          
-            Users.create({
-                    ...req.body,
-                    password: bcryptjs.hashSync(req.body.password, 10), //como estoy en un objeto, esta contrasena va a pisar a la que viene en el body
-                    confirm_password: bcryptjs.hashSync(req.body.confirm_password, 10),
-                    image: req.file.filename,
-                    status: "A"
-                  
-
+                .then((resultado) => {
+                    userInDB = resultado
                 })
-                .then(() => res.redirect('/usuarios/login'))
+                .catch(err => res.send(err))
+
+            //console.log("el usuario esta en db?***", userInDB);
+
+            if (resultValidation.errors.length > 0) {
+                res.render('users/register', {
+                    errors: resultValidation.mapped(),
+                    oldData: req.body
+                });
+            } else if (userInDB) {
+                return res.render('users/register', {
+                    errors: {
+                        email: {
+                            msg: 'Este email ya está registrado'
+                        }
+                    },
+                    oldData: req.body
+                });
+            } else {
+                Users.create({
+                        ...req.body,
+                        password: bcryptjs.hashSync(req.body.password, 10), //como estoy en un objeto, esta contrasena va a pisar a la que viene en el body
+                        confirm_password: bcryptjs.hashSync(req.body.confirm_password, 10),
+                        image: req.file.filename,
+                        status: "A"
+
+
+                    })
+                    .then(() => res.redirect('/usuarios/login'))
+                    .catch(err => {
+                        res.send(err)
+                    })
+            }
+        },
+
+        edit: function (req, res) {
+            const id = req.params.id
+            // Users.findByPk(id,{
+            //     include: [{association: "rols"}]
+            // })
+            // .then( user => res.render("users/edit", {user}))
+            let pedidoUsuario = Users.findByPk(id);
+            let pedidoRoles = Rols.findAll();
+
+            Promise.all([pedidoUsuario, pedidoRoles])
+                .then(([user, rol]) => {
+                    res.render("users/edit", {
+                        user: user,
+                        rol: rol
+                    })
+                })
                 .catch(err => {
                     res.send(err)
                 })
-        }
-    },
 
-    edit: function(req,res){
-        const id = req.params.id
-        // Users.findByPk(id,{
-        //     include: [{association: "rols"}]
-        // })
-        // .then( user => res.render("users/edit", {user}))
-        let pedidoUsuario = Users.findByPk(id);
-        let pedidoRoles = Rols.findAll();
-
-        Promise.all([pedidoUsuario, pedidoRoles])
-            .then(([user, rol]) => {
-                res.render("users/edit", {user:user, rol:rol})
-            })
-            .catch(err => {
-                res.send(err)
-            })
-
-    },
-    actualizar: (req, res) => {
-        // Chequear si en la edicion se pone una imagen nueva o el campo viene vacio
-        let finalImage;
-        if (!req.file) {            
-            finalImage = req.session.userLogged.image            
-        } else {
-            finalImage = req.file.filename
-        }
-        
-        Users.update({
-            ...req.body,
-            image: finalImage,
-            rols_id: req.body.rol
-        }, {
-            where: {
-                id: req.params.id
+        },
+        actualizar: (req, res) => {
+            // Chequear si en la edicion se pone una imagen nueva o el campo viene vacio
+            let finalImage;
+            if (!req.file) {
+                finalImage = req.session.userLogged.image
+            } else {
+                finalImage = req.file.filename
             }
-        })
-        .then(() => res.redirect('/usuarios/detalle/' + req.params.id))
-        .catch(err => res.render(err))
-    },
-    detalle: function(req,res){
-        const id = req.params.id
-        Users.findByPk(id, {
-            include: [{association: "rols"}]
-        })
-        .then( user => res.render("users/details", {user}))
-        .catch(err => {
-            res.send(err)
-        })
-    },
 
-};
+            Users.update({
+                    ...req.body,
+                    image: finalImage,
+                    rols_id: req.body.rol
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(() => res.redirect('/usuarios/detalle/' + req.params.id))
+                .catch(err => res.render(err))
+        },
+        detalle: function (req, res) {
+            const id = req.params.id
+            Users.findByPk(id, {
+                    include: [{
+                        association: "rols"
+                    }]
+                })
+                .then(user => res.render("users/details", {
+                    user
+                }))
+                .catch(err => {
+                    res.send(err)
+                })
+        },
+        // metodos de la API
+        list: (req, res) => {
+            Users.findAll()
+                .then(usersDB => {
+                    let users = [];
+                    usersDB.forEach(user => {
+                        let userForApi = {
+                            id: user.id,
+                            name: user.first_name,
+                            lastName: user.last_name,
+                            email: user.email,
+                            detail: `/api/users/${user.id}`,
+                            image: `/images/users/${user.image}`
+                        };
+                        users.push(userForApi);
+                    });
+                    let respuesta = {
+                        meta: {
+                            status: 200,
+                            url: 'api/users'
+                        },
+                        total: users.length,
+                        usuarios: users
+                    }
+                    res.json(respuesta);
+                }).catch(error =>
+                    res.send(error)
+                )
+        },
+        apiDetail: function (req, res) {
 
-module.exports = userController;
+            Users.findByPk(req.params.id)
+                .then(user => {
+
+                    let userForApi = {
+                        id: user.id,
+                        name: user.first_name,
+                        lastName: user.last_name,
+                        email: user.email,
+                        detail: `/api/users/${user.id}`,
+                        image: `/images/users/${user.image}`
+                    };
+
+                    res.json(userForApi);
+
+                }).catch(error =>
+                    res.send(error)
+                )
+        }
+    }
+        module.exports = userController;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController')
+const userController = require("../controllers/userControllers")
 
 // Generar una ruta, en este caso raiz
 router.get('/', mainController.index);
 router.get('/contactenos', mainController.contactenos)
+
+router.get('/api/users', userController.list);
+router.get('/api/users/:id', userController.apiDetail);
 
 
 module.exports = router;
