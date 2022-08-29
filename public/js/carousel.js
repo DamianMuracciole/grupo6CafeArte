ea0516802e31fff56df5725f0c0c976526cf7655
caguille <cristianma26@gmail.com>
2022-08-29 01:14:10 -0300

    prueba de carrusel

---
+++ public/js/carousel.js
@@ -0,0 +1,50 @@
+addEventListener('DOMContentLoaded',() =>{
+const imagenes = ['/images/tomemonos-un-cafe-banner.jpg','/images/carrusel-img-1.jpg', '/images/carrusel-img-2.jpg', '/images/carrusel-img-3.jpg']
+
+
+
+let i= 1
+const img1 = document.querySelector('#img1')
+const img2 = document.querySelector('#img2')
+const progerssBar = document.querySelector('#progress-bar')
+const divIndicadores = document.querySelector('#indicadores')
+
+let porcentaje_base= 100/imagenes.length
+let porcentaje_actual = porcentaje_base
+
+for (let index = 0; index < imagenes.length; index++) {
+    const div = document.createElement('div')
+    div.classList.add('circles')
+    div.id = index
+    divIndicadores.appendChild(div)
+}
+
+progerssBar.style.width = `${porcentaje_base}`
+img1.src = imagenes[0]
+const circulos = document.querySelectorAll('.circles')
+circulos[0].classList.add('resaltado')
+
+const slideshow = () =>{
+    img2.src = imagenes[i]
+    const circulo_actual = Array.from (circulos).find(el => el.id == i)
+    Array.from(circulos).forEach(cir => cir.classList.remove('resaltado'));
+    circulo_actual.classList.add('resaltado')
+
+    img2.classList.add('active')
+    i++
+    porcentaje_actual+=porcentaje_base
+    progerssBar.style.width = `${porcentaje_actual}%`
+    if (i == imagenes.length){
+        i=0
+        porcentaje_actual= porcentaje_base - porcentaje_base
+    }
+
+    setTimeout(() => {
+        img1.src = img2.src
+        img2.classList.remove('active')
+    }, 1000)
+}
+
+setInterval(slideshow, 4000)
+
+})
